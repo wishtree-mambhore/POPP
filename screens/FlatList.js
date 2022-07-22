b8761275c7d9e7d1c0dafb85ebd4f446e53e26d1
Mrunali Ambhore <102215326+wishtree-mambhore@users.noreply.github.com>
2022-07-22 18:27:31 +0530

    Build logic for search bar

--- screens/FlatList.js
+++ screens/FlatList.js
@@ -1,11 +1,39 @@
 import { View, Text ,FlatList} from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import css from '../style/GlobalStyle'
 import Colors from '../style/Colors'
 
-const CustomFlatList = ({data}) => {
-    
+const CustomFlatList = ({data,text}) => {
+  console.log(text)
+    const [searchText,setsearchText]=useState(text)
+    const [searchData,setsearchData]=useState(data)
+    const [masterDataSource, setMasterDataSource] = useState(data);
+
    
+  const searchFilterFunction = (text) => {
+    // Check if searched text is not blank
+    if (text) {
+      // Inserted text is not blank
+      // Filter the masterDataSource
+      // Update FilteredDataSource
+      const newData = searchData.filter(function (item) {
+        const itemData = item.title
+          ? item.title.toUpperCase()
+          : ''.toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setsearchData(newData);
+      setsearchText(text);
+    } else {
+      // Inserted text is blank
+      // Update FilteredDataSource with masterDataSource
+      setsearchData(masterDataSource);
+      setSearch(text);
+    }
+  };
+
+
   return (
   <FlatList
   data={data}
